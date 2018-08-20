89931098dc7524d4f70a7e547a3381ab6540458e
Hirotaka Onaka <hiro.onaka0510@gmail.com>
2018-08-20 16:56:58 +0000

    mount SENDGRID

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   root to: "blogs#top"
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :favorites, only:[:create, :destroy]
   resources :sessions, only: [:new, :create, :destroy]
   resources :users
   resources :blogs do
     collection do
       post :confirm
-      mount LetterOpenerWeb::Engine, at:"/letter_opener" if Rails.env.development?
     end
   end
 end
